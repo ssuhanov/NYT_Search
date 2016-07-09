cdf794889f1e762b8b98f5b80ebc2242764ad72c
Serge Sukhanov <ssuhanov@icloud.com>
2016-07-09 19:02:13 +0300

    Added controllers for tableView and cell

---
+++ NYT_Search/ArticleCell.swift
@@ -0,0 +1,26 @@
+//
+//  ArticleCell.swift
+//  NYT_Search
+//
+//  Created by Serge Sukhanov on 09.07.16.
+//  Copyright © 2016 Serge Sukhanov. All rights reserved.
+//
+
+import UIKit
+
+class ArticleCell: UITableViewCell {
+    
+    var article: Article?
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
